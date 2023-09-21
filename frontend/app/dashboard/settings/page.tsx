000dc251b935291c61f46ed1e1a8918555841d37
doch2130 <doch2130@gmail.com>
2023-09-21 18:32:36 +0900

    feat: 2023-09-21 1830 next js start

---
+++ frontend/app/dashboard/settings/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h2>hello next.js Settings</h2>
+    </div>
+  )
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Next.js',
+}
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, Home page!</h1>
+    </div>
+  )
+}
